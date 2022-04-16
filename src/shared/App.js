1ba317015451a2b30090b660237c5f8a4e621b06
douchebag1108 <cjy653@naver.com>
2022-04-16 22:46:30 +0900

    10:46

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
 import { useDispatch } from "react-redux";
-import { Button, Grid } from "../elements";
+import { Grid } from "../elements";
 import styled from "styled-components";
 // import Header from "../components/Header";
 // import PostList from "../pages/PostList";
@@ -21,31 +21,14 @@ function App() {
 
   return (
     <React.Fragment>
-      <Container>
         <Grid>
-          {/* <Header></Header> */}
-
           <ConnectedRouter history={history}>
-            <Route path="/" exact component={Login} />
-            <Route path="/signUp" exact component={SignUp} />
-
-            {/* <Route path="/postList" exact component={PostList} />
-            <Route path="/post/:id" exact component={PostDetail} />
-            <Route path="/post" exact component={PostWrite} />
-            <Route path="/modify/:id" exact component={PostWrite} /> */}
-
+            <Route path="/" exact component={Login}/>
+            <Route path="/signUp" exact component={SignUp}/>
           </ConnectedRouter>
-          
         </Grid>
-      </Container>
     </React.Fragment>
   );
 }
 
 export default App;
-
-const Container = styled.div`
-  width: 1200px;
-  margin: 0 auto;
-  height: 100%;
-`;
